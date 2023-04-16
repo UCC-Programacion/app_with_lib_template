ec606e667e7a94019cb8d97a4b045d82a2e320c0
Matias Volpe <kratico@users.noreply.github.com>
2023-04-16 20:52:58 -0300

    Create cmake.yml

--- apps/main_app1.cpp
+++ apps/main_app1.cpp
@@ -2,7 +2,7 @@
 #include <mymath/my_math.h>
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  std::cout << "App1!!!" << std::endl;
 
   std::cout << mymath::sum(1, 2) << std::endl;
 
---
+++ apps/main_app2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <mymath/my_math.h>
+
+int main() {
+  std::cout << "App2!!!" << std::endl;
+
+  std::cout << mymath::sum(3, 4) << std::endl;
+
+  return EXIT_SUCCESS;
+}
